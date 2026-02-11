4da795678089271d176f2bff6535002b70573f30
Svenpire <svenpire@gmail.com>
2026-02-11 04:42:56 -0700

    update nav with mentorship, profile & profile pages (empty index)

---
+++ MentoringApp.Ui/Pages/Mentorships/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MentoringApp.Ui.Pages.Mentorships
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
